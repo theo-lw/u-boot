8fd30af816c296776169523c202edeb0465539e2
Theodore Luo Wang <wangtheo662@gmail.com>
2023-03-04 14:03:02 -0500

    Just do what we're doing in boot.S

--- arch/arm/include/asm/system.h
+++ arch/arm/include/asm/system.h
@@ -20,6 +20,30 @@
 #define ES_TO_AARCH64		1
 #define ES_TO_AARCH32		0
 
+// ***************************************
+// SCTLR_EL1, System Control Register (EL1)
+// Architecture Reference Manual Section D13.2.118
+// ***************************************
+#define SCTLR_RESERVED (3 << 28) | (3 << 22) | (1 << 20) | (1 << 11)
+#define USER_MASK_ACCESS (1 << 9)
+#define SCTLR_WFE_WFI_ENABLED (1 << 18 | 1 << 16)
+#define SCTLR_VALUE_MMU_DISABLED                                               \
+  (SCTLR_RESERVED | USER_MASK_ACCESS | SCTLR_WFE_WFI_ENABLED)
+
+// ***************************************
+// HCR_EL2, Hypervisor Configuration Register (EL2)
+// Architecture Reference Manual Section D13.2.48
+// ***************************************
+#define HCR_RW (1 << 31)
+
+// ***************************************
+// SPSR_EL3, Saved Program Status Register (EL3)
+// Architecture Reference Manual Section C5.2.20
+// ***************************************
+#define SPSR_MASK_ALL (7 << 6)
+#define SPSR_EL1h (5 << 0)
+#define SPSR_VALUE (SPSR_MASK_ALL | SPSR_EL1h)
+
 /*
  * SCR_EL3 bits definitions
  */
