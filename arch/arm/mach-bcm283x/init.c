d85882cf083e31076403ae9b25f7011addef4ba0
Theodore Luo Wang <wangtheo662@gmail.com>
2023-03-04 20:35:48 -0500

    Fix data cache??

--- arch/arm/mach-bcm283x/init.c
+++ arch/arm/mach-bcm283x/init.c
@@ -53,7 +53,7 @@ static struct mm_region bcm2711_mem_map[MEM_MAP_MAX_ENTRIES] = {
          PTE_BLOCK_MEMTYPE(MT_NORMAL) | PTE_BLOCK_INNER_SHARE | PTE_BLOCK_RW},
     {.virt = 0xfc000000UL,
      .phys = 0xfc000000UL,
-     .size = 0x03800000UL,
+     .size = 0x03c00000UL,
      .attrs = PTE_BLOCK_MEMTYPE(MT_DEVICE_NGNRNE) | PTE_BLOCK_NON_SHARE |
               PTE_BLOCK_PXN | PTE_BLOCK_UXN | PTE_BLOCK_RW},
     {.virt = BCM2711_RPI4_PCIE_XHCI_MMIO_PHYS,
